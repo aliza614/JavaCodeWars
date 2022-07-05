6c629a323c3c9ff6505bb2583c39ebaf0c57f379
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-05 16:31:50 -0500

    problem solved

--- src/ConvertStringToCamelCase.java
+++ src/ConvertStringToCamelCase.java
@@ -2,17 +2,20 @@ import java.lang.StringBuilder;
 public class ConvertStringToCamelCase {
 
 
+
+
         static String toCamelCase(String s){
             StringBuilder sb=new StringBuilder(s);
             for(int i=0;i<sb.length()-1;i++){
 
                 if(sb.charAt(i)==' '||sb.charAt(i)=='-'||sb.charAt(i)=='_')
                 {
-                    sb.setCharAt(i+1,sb.charAt(i+1));
+                    sb.setCharAt(i+1,Character.toUpperCase(sb.charAt(i+1)));
                     sb.deleteCharAt(i--);
                 }
             }
             return sb.toString();
         }
+
 }
 
