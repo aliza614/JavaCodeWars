75f95beebe3de2156963454b62fdf2b72ceef076
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-05 19:11:39 -0500

    almost solved

---
+++ src/SupermarketQueue.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+public class SupermarketQueue {
+
+        public static int solveSuperMarketQueue(int[] customers, int n) {
+            int answer=0;
+            if (customers.length==0) return 0;
+            for (int customer:
+                 customers) {
+                answer+=customer;
+
+            }
+            Arrays.sort(customers);
+            return Math.max(answer/n,customers[customers.length-1]);
+
+        }
+
+}
+
